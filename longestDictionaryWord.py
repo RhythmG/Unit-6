10f4e5a71309e7fd586690cc72f97bb690aa4ad5
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-10 08:20:58 -0400

    Updated from Brython Server: 5/10/2018 8:20:57 AM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,13 @@
+#Stephen Wang
+#5/10/18
+#Prints out the longest word in the dictionary
 
+file = open('engmix.txt')
+word = ''
+
+for line in file:
+    if len(line)> len(word):
+        word = line
+
+print(word)
+            
