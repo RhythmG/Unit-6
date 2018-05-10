a2d586b912f2d892f93b69d24c6e863fd0677e80
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-10 08:30:26 -0400

    Updated from Brython Server: 5/10/2018 8:30:25 AM

--- howManyWords.py
+++ howManyWords.py
@@ -1,1 +1,15 @@
+#Stephen Wang
+#5/10/18
+#how many words there are for each numbers of letters in the dictionary file
 
+file = open('engmix.txt')
+nums = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for line in file:
+    for i in range(1, 23):
+        if len(line) == i:
+            nums[i-1] += 1
+            
+print(nums)
+        
+            
