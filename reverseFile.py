6a1ac06ded019ebb501ab656cf0754a28321d992
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-10 08:49:21 -0400

    Updated from Brython Server: 5/10/2018 8:49:20 AM

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,11 @@
+#Stephen Wang
+#5/10/18
+#Asks the user for the name of a file and prints the lines in reverse
 
+ask = input("Enter the name of a file: ")
+file = open(ask)
+
+for line in file:
+    words = line.split()
+    for i in range(len(line), -1):
+        print(words[i])
