71cfe29a19e2946912bd784511a04c4896449d92
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-10 08:22:21 -0400

    Update zzwords.py

--- zzwords.py
+++ zzwords.py
@@ -1,1 +1,13 @@
+#Stephen Wang
+#5/10/18
+#Prints out all the words in the dictionary that contain zz
+
+file = open('engmix.txt')
+
+for line in file:
+    if 'zz' in line:
+        print(line.strip())
+
+            
+
 
