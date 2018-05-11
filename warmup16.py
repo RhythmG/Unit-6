92d12dbb064cf274b2f74e99713809311320a075
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-11 08:29:57 -0400

    Updated from Brython Server: 5/11/2018 8:29:56 AM

--- warmup16.py
+++ warmup16.py
@@ -1,3 +1,12 @@
 #Stephen Wang
 #5/11/18
 #Print all words that start with your first initial and end with your last initial
+
+file = open('engmix.txt')
+
+
+for word in file:
+    word = word.strip()
+    if word[0] == 's' and word[len(word)-1]  == 'w':
+        print(word)
+        
