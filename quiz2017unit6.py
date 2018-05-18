fccae3af81b8deef156ae2f9b5f855b6434d1008
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-18 08:48:32 -0400

    Updated from Brython Server: 5/18/2018 8:48:31 AM

--- quiz2017unit6.py
+++ quiz2017unit6.py
@@ -3,9 +3,25 @@
 #Quiz from last year
 
 file = open('engmix.txt')
+rwords = 0
 
+"""
 for line in file:
     line = line.strip()
     if line.count('c') == 3 and line.count('p') == 2:
         print(line)
+"""
 
+"""        
+for line in file:
+    if len(line) > 0 and 'r' == line[0]:
+        rwords += 1
+print(rwords)
+"""
+
+num = int(input('Enter a number: '))
+
+for line in file:
+    if len(line) == num+1:
+        print(line)
+        break
