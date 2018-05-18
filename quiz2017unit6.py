329fa85b81605e0501b431cdfd3b248fd4f72b12
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-18 08:33:01 -0400

    Updated from Brython Server: 5/18/2018 8:33:00 AM

--- quiz2017unit6.py
+++ quiz2017unit6.py
@@ -1,1 +1,12 @@
+#Stephen Wang
+#5/18/18
+#Quiz from last year
+
+file = open('engmix.txt')
+words = []
+
+for line in file:
+    line = line.strip()
+    if line.count('c') == 3 and line.count('p') == 2:
+        print(line)
 
