239dcaa10a8a43c8591e0f5140924fc9820b5289
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-23 08:39:52 -0400

    Update dotsandboxes.py

--- dotsandboxes.py
+++ dotsandboxes.py
@@ -1,1 +1,35 @@
+#Stephen Wang
+#5/23/18
+#Final project - Dots and boxes
+
+from ggame import *
+
+XSLOT = 300
+YSLOT = 100
+
+red = Color(0xFF0000, 1)
+blue = Color (0x0000FF, 1)
+black = Color (0x000000, 1)
+
+blackOutline = LineStyle(5, black)
+vblackLine = LineAsset(0,80, blackOutline)
+hblackLine = LineAsset(45, 0, blackOutline)
+
+def buildBoard(a):
+    for r in range(0,4):
+        for c in range(0,4):
+            print(board[r][c], end =' ')
+        print()
+    
+row1 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+row2 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+row3 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+row4 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+
+
+board[row-1][col-1] = 1
+
+
+
+App().run()
 
