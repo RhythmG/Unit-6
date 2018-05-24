689ab5333e1c3dfdd3af54be17dad510ea4b1962
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-24 11:18:35 -0400

    Update dotsandboxes.py

--- dotsandboxes.py
+++ dotsandboxes.py
@@ -4,32 +4,47 @@
 
 from ggame import *
 
-XSLOT = 300
-YSLOT = 100
+XSLOT = 200
+YSLOT = 80
+x = 0
+y = 0
 
-red = Color(0xFF0000, 1)
-blue = Color (0x0000FF, 1)
-black = Color (0x000000, 1)
-
-blackOutline = LineStyle(5, black)
-vblackLine = LineAsset(0,80, blackOutline)
-hblackLine = LineAsset(45, 0, blackOutline)
-
-def buildBoard(a):
-    for r in range(0,4):
-        for c in range(0,4):
-            print(board[r][c], end =' ')
-        print()
+def buildBoard():
+    board = [[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]] 
     
-row1 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-row2 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-row3 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
-row4 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+def RedrawAll():
+    x = 0 
+    y = 0
+    for item in App().spritelist[:]:
+        item.destroy()
+    for r in range(0,4):
+            for c in range(0,2):
+                x = x + 50
+                Sprite(vblackLine,(XSLOT + 50, YSLOT + y)) #left edge
+                Sprite(vblackLine,(XSLOT + 478, YSLOT + y)) #right edge
+                y = y + 50
+                Sprite(hblackLine,(XSLOT + x, YSLOT)) #top edge
+                Sprite(hblackLine,(XSLOT + x, YSLOT + 427)) #bottom edge
+            print()
+            
+"""
+def LeftEdge(a, b, c, d):
+"""
+       
 
-
-board[row-1][col-1] = 1
-
-
-
-App().run()
-
+if __name__ == '__main__': 
+    red = Color(0xFF0000, 1)
+    blue = Color (0x0000FF, 1)
+    black = Color (0x000000, 1)
+    
+    blackOutline = LineStyle(5, black)
+    vblackLine = LineAsset(0,80, blackOutline)
+    hblackLine = LineAsset(80, 0, blackOutline)
+    
+    App().run()
+    RedrawAll()
+    """
+    LeftEdge(XSLOT, YSLOT, x, y)
+    """
+    
+    
