3812b771eba34395792f609b5e9c8b26eb7eb798
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-29 09:23:29 -0400

    Update RAMCalc.py

--- RAMCalc.py
+++ RAMCalc.py
@@ -5,26 +5,65 @@
 
 #Enter your function after 'return' in line 9: (use ** to denote an exponent)
 
+print('1: Rectangles')
+print('2: Trapezoid')
+print("3: Simpson's Rule")
+
+typeapprox = int(input('Enter a number corresponding to your desired approximation: '))
+
 def f(x):
     return x**3
 
-  
-lower = float(input('Enter a lower bound: '))
-upper = float(input('Enter an upper bound: '))
-intervals = int(input('Enter a number of rectangles to approximate: '))
-
-
-def integral(a, b, numofrectangles):
-    width = (b-a)/numofrectangles
-    sum = 0
-    for i in range(numofrectangles):
-        height = f(a + i*width)
-        area = height*width
-        sum += area
+if typeapprox == 1:
     print('')
-    print(sum)
+    lower = float(input('Enter a lower bound: '))
+    upper = float(input('Enter an upper bound: '))
+    intervals = int(input('Enter a number of rectangles to approximate: '))
     
-
-integral(lower, upper, intervals)
-
+    def integral(a, b, numofrectangles):
+        width = (b-a)/numofrectangles
+        sum = 0
+        for i in range(numofrectangles):
+            height = f(a + i*width)
+            area = height*width
+            sum += area
+        print('')
+        print(sum)
     
+    integral(lower, upper, intervals)
+
+if typeapprox == 2:
+    print('')
+    lower = float(input('Enter a lower bound: '))
+    upper = float(input('Enter an upper bound: '))
+    intervals = int(input('Enter a number of trapezoids to approximate: '))
+    def trapezoid(a, b, numoftraps):
+        width = (b-a)/numoftraps
+        sum = 0
+        sum += f(a) + f(b)
+        for i in range(numoftraps-2):
+            height = 2*f(a + i*width)
+            area = height*width
+            sum += area
+        print('')
+        print(sum)
+    
+    trapezoid(lower, upper, intervals)
+
+if typeapprox == 3:
+    print('')
+    lower = float(input('Enter a lower bound: '))
+    upper = float(input('Enter an upper bound: '))
+    intervals = int(input('Enter a number of Simpson shapes to approximate: '))
+    def simpson(a, b, numoftraps):
+        width = (b-a)/numoftraps
+        sum = 0
+        for i in range(numoftraps):
+            height = f(a + i*width)
+            area = height*width
+            sum += area
+        print('')
+        print(sum)
+    
+    simpson(lower, upper, intervals)
+         
