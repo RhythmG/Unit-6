61c410cbc089402ee58c2ae49a22e723f27ceb06
swang99 <35492791+swang99@users.noreply.github.com>
2018-05-30 08:40:14 -0400

    Updated from Brython Server: 5/30/2018 8:40:13 AM

--- dotsandboxes.py
+++ dotsandboxes.py
@@ -7,18 +7,12 @@ from ggame import *
 XSLOT = 200
 YSLOT = 80
 
-
-
 def buildBoard():
     return [[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],[[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]] 
 
-"""
-pegs = EllipseAsset(10,50, blackOutline, black)
-"""
-
 def RedrawAll():
-    x = 0
-    y = 0
+    data['x'] = 0
+    data['y'] = 0
     blackOutline = LineStyle(5, black)
     vblackLine = LineAsset(0,80, blackOutline)
     hblackLine = LineAsset(80, 0, blackOutline)
@@ -26,19 +20,56 @@ def RedrawAll():
         item.destroy()
     for r in range(0,4):
             for c in range(0,2):
-                x = x + 50
-                Sprite(vblackLine,(XSLOT + 50, YSLOT + y)) #left edge
-                Sprite(vblackLine,(XSLOT + 478, YSLOT + y)) #right edge
-                y = y + 50
-                Sprite(hblackLine,(XSLOT + x, YSLOT)) #top edge
-                Sprite(hblackLine,(XSLOT + x, YSLOT + 427)) #bottom edge
+                pegs = EllipseAsset(10,50, blackOutline, black)
+                data['x'] = data['x'] + 50
+                """
+                Sprite(vblackLine,(XSLOT + 50, YSLOT + data['y'])) #left edge
+                Sprite(vblackLine,(XSLOT + 478, YSLOT + data['y'])) #right edge
+                data['y'] = data['y'] + 50
+                Sprite(hblackLine,(XSLOT + data['x'], YSLOT)) #top edge
+                Sprite(hblackLine,(XSLOT + data['x'], YSLOT + 427)) #bottom edge
+                """
             print()
             
-"""
-def LeftEdge(a, b, c, d):
+def LeftEdge(startx, starty, currentx, currenty):
+    data['x'] = 0
+    data['y'] = 0
+    c = 0
+    for r in range(0,4):
+        if data['board'][r][c] == 0:
+            Sprite(vblackLine,(XSLOT + 50, YSLOT + data['y']))
+            c = c+1
 
-"""
-       
+def RightEdge(startx, starty, currentx, currenty):
+    XSLOT = 250
+    data['x'] = 0
+    data['y'] = 0
+    c = 0
+    for r in range(0,4):
+        if data['board'][r][c] == 0:
+            Sprite(vblackLine,(XSLOT + 478, YSLOT + data['y']))
+            c = c+1
+
+def UpperEdge(startx, starty, currentx, currenty):
+    data['x'] = 0
+    data['y'] = data['y'] + 50
+    c = 0
+    for r in range(0,4):
+        if data['board'][r][c] == 0:
+            Sprite(hblackLine,(XSLOT + data['x'], YSLOT))
+            c = c+1
+            
+def LowerEdge(startx, starty, currentx, currenty):
+    YSLOT = 130
+    data['x'] = 0
+    data['y'] = 0
+    c = 0
+    for r in range(0,4):
+        if data['board'][r][c] == 0:
+            Sprite(hblackLine,(XSLOT + data['x'], YSLOT + 427))
+            c = c+1
+
+board = buildBoard()
 
 if __name__ == '__main__': 
     red = Color(0xFF0000, 1)
@@ -46,13 +77,14 @@ if __name__ == '__main__':
     black = Color (0x000000, 1)
     data = {}
     data['board'] = buildBoard()
+    data['x'] = 0
+    data['y'] = 0
     
     
     
     App().run()
     RedrawAll()
-    """
-    LeftEdge(XSLOT, YSLOT, x, y)
-    """
+    LeftEdge(XSLOT, YSLOT, data['x'], data['y'])
+    RightEdge(XSLOT, YSLOT, data['x'], data['y'])
     
     
