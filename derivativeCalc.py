c708cb0fc6f66448dd8829a00b1f2ad0eae85ad7
swang99 <35492791+swang99@users.noreply.github.com>
2018-06-02 19:59:05 -0400

    Updated from Brython Server: 6/2/2018 7:59:05 PM

--- derivativeCalc.py
+++ derivativeCalc.py
@@ -2,7 +2,16 @@
 #Calculus 10 Essential Problems Project - Chapter 3
 #Derivative Calculator
 
+xvalue = float(input('Enter a value of x to approximate the derivative :'))
 def f(x):
-    return ((x-2)**2)*(x-4)
+    return ((x-2)**2)*(x-4) #This is the function used to approximate the derivative
+
 def derivative(x):
-    h = 1./1000.
+    h = 1/1000
+    rise = f(x+h)-f(x)
+    run = h
+    slope = rise/run
+    return slope
+
+print('')
+print('Derivative at x =', xvalue, ':', derivative(3))
