98e72ee158a5ff6747d190bc1f549edaec7d7cfe
swang99 <35492791+swang99@users.noreply.github.com>
2018-06-07 17:33:44 -0400

    Updated from Brython Server: 6/7/2018 5:33:43 PM

--- derivativeCalc.py
+++ derivativeCalc.py
@@ -9,15 +9,14 @@ xvalue = float(input('Enter a value of x to approximate the derivative :')) #at 
 def f(x):
     return abs(x) #This is the function used to approximate the derivative
 
-try:
-    def derivative(x):
-        h = 1/1000
-        rise = f(x+h)-f(x) 
-        run = h
-        slope = rise/run #definition of derivative
-        return slope
-except:
-    print("The function is not differentiable at x = ", xvalue)
+def derivative(x):
+    h = 1/1000
+    rise = f(x+h)-f(x) 
+    run = h
+    slope = rise/run #definition of derivative
+    return slope
+
+print("The function is not differentiable at x = ", xvalue)
 
 print('')
 print('Derivative at x =', xvalue, ':', round(derivative(xvalue), 2)) #Prints out the value of the derivative
