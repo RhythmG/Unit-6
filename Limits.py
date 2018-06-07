f60fd55b3f6cb3888040078380a7eea828acdbcf
swang99 <35492791+swang99@users.noreply.github.com>
2018-06-07 11:46:31 -0400

    Updated from Brython Server: 6/7/2018 11:46:28 AM

--- Limits.py
+++ Limits.py
@@ -1,1 +1,19 @@
+#Stephen Wang
+#Calculus Programming Week
+#Limit calculator
 
+from math import *
+
+approaching = float(input('Enter a x-value to approach: '))
+
+def f(x):
+    return (x**2)/(x-2)
+
+def limit(x):
+    if f(approaching) <= 1.0E16 and f(approaching) >= -1.0E16:
+        print(f(approaching))
+    elif f(approaching-0.0001) - f(approaching+0.0001) <= 0.1:
+            print((f(approaching-0.0001) + f(approaching+0.0001))/2)
+
+limit(approaching)
+    
