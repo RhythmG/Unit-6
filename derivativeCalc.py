a1d97430ce01ca7928a2574239fe259d43140418
swang99 <35492791+swang99@users.noreply.github.com>
2018-06-07 18:29:09 -0400

    Updated from Brython Server: 6/7/2018 6:29:09 PM

--- derivativeCalc.py
+++ derivativeCalc.py
@@ -3,20 +3,13 @@
 #Derivative Calculator
 
 from math import *
-import sys
 
 xvalue = float(input('Enter a value of x to approximate the derivative :')) #at what value of x for the derivative?
+
 
 def f(x):
     return abs(x) #This is the function used to approximate the derivative
 
-def checksides(x): #This is to check if the one-sided derivatives are equal
-    h = 1/1000
-    rise = f(x+h)-f(x) 
-    run = h
-    slope = rise/run 
-    if rise - (f(x-h)-f(x)) <= h:
-        print("The function is not differentiable at this point.")
 
 def derivative(x):
     h = 1/1000
@@ -26,7 +19,5 @@ def derivative(x):
     return slope
 
 
-
 print('')
-checksides(xvalue)
 print('Derivative at x =', xvalue, ':', round(derivative(xvalue), 2)) #Prints out the value of the derivative
