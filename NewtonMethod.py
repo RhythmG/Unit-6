147a13f1623cf360500f444349f6f0fbdeac66c0
swang99 <35492791+swang99@users.noreply.github.com>
2018-06-11 11:40:46 -0400

    Updated from Brython Server: 6/11/2018 11:40:49 AM

--- NewtonMethod.py
+++ NewtonMethod.py
@@ -2,8 +2,10 @@
 #Calculus Programming Week
 #Newton's Method
 
+from math import * 
+
 def f(x):
-    return (x**3)-(7*x**2)+(8*x)-3
+    return x*cos(x) - (x**2)
 
 order = int(input('Enter a number of iterations to estimate the point: '))
 guess = float(input('Enter an initial point: '))
@@ -12,8 +14,9 @@ guess = float(input('Enter an initial point: '))
 def NewtonMethod(a):
     estimation = 0
     for i in range(1, (order+1)):
-        estimation += (a - (f(a)/derivative(a)))
-    print(estimation)
+        estimation = (a - (f(a)/derivative(a)))
+        a = estimation 
+        print(estimation)
 
 def derivative(a):
     h = 1/1000
