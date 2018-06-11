f90ef0fcfea0ccea7a6ba82e19dc329e85049512
swang99 <35492791+swang99@users.noreply.github.com>
2018-06-11 12:00:49 -0400

    Updated from Brython Server: 6/11/2018 12:00:50 PM

--- lenofcurve.py
+++ lenofcurve.py
@@ -1,1 +1,27 @@
+#Stephen Wang + Maia Reynolds
+#Calculus Programming Week
+#Length of a Curve
+
+from math import *
+
+def rectangles(a, b, numofrectangles):
+        width = (b-a)/numofrectangles
+        sum = 0
+        for i in range(numofrectangles):
+            if ramselect == 1:
+                i0 = i
+            elif ramselect == 2:
+                i0 = i+1
+            elif ramselect == 3:
+                i0 = i+0.5
+            else:
+                print("Error -- input is not in range")
+            height = f(a + i0*width)
+            area = height*width
+            sum += area
+        print('')
+        print('Area: ', sum)
+    
+    rectangles(lower, upper, intervals)
+
 
