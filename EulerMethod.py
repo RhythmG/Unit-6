e4d22817de9504161d83fe182509fabbde197b29
swang99 <35492791+swang99@users.noreply.github.com>
2018-06-13 11:23:32 -0400

    Updated from Brython Server: 6/13/2018 11:23:33 AM

--- EulerMethod.py
+++ EulerMethod.py
@@ -1,1 +1,27 @@
+#Stephen and Maia
+#Calculus Programming Week
+#Euler's Method
 
+from math import *
+
+def f(x):
+    return sin(x)
+
+def derivative(x):
+    h = 1/1000
+    rise = f(x+h)-f(x) 
+    run = h
+    slope = rise/run 
+    return slope
+
+initialx = int(input('Enter an initial x value: '))
+dx=float(input("Enter a dx value: "))
+finalx=int(input("Enter a final x value: "))
+
+def euler():
+    while x!=finalx:
+        y=f(initialx)
+        dy=derivative(y)*dx
+        y+=dy
+        x+=dx
+        print("(",x,",",y,")"
