6022d4b13d847088df4f378456bba74ccc840837
swang99 <35492791+swang99@users.noreply.github.com>
2018-09-07 10:34:54 -0400

    Updated from Brython Server: 9/7/2018 10:34:55 AM

--- mmproj1.py
+++ mmproj1.py
@@ -1,6 +1,9 @@
 from random import randint
 
 notlookedat = 0
+j= 0
+k= 0
+l= 0
 
 L = [1]
 n = int(input('Enter a number of birds: '))
@@ -11,14 +14,20 @@ for i in range(1, n+1):
 L.append(2)
 print(L)
 
-for i in range(1, n+1):
+for i in range(1, n):
     if L[i-1] == 2 and L[i+1] == 1:
         notlookedat = notlookedat + 1
-    elif L[1] == 1:
+        j = j+1
+    elif L[0] == 1 and L[1] == 1:
         notlookedat = notlookedat + 1
-    elif L[n-1] == 2:
+        k = k+1
+    elif L[n+1] == 2 and L[n] == 2:
         notlookedat = notlookedat + 1
+        l = l+1
 
-print(notlookedat)
+print("Not looked at:", notlookedat)
+print(j)
+print(k)
+print(l)
 
 
