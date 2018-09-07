22143ca62afa13bb418aec188f43d09132ffba62
swang99 <35492791+swang99@users.noreply.github.com>
2018-09-07 10:23:24 -0400

    Updated from Brython Server: 9/7/2018 10:23:24 AM

--- mmproj1.py
+++ mmproj1.py
@@ -14,6 +14,10 @@ print(L)
 for i in range(1, n+1):
     if L[i-1] == 2 and L[i+1] == 1:
         notlookedat = notlookedat + 1
+    elif L[1] == 1:
+        notlookedat = notlookedat + 1
+    elif L[n-1] == 2:
+        notlookedat = notlookedat + 1
 
 print(notlookedat)
 
