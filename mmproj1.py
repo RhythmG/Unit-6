113a69ab8d751affaf5d0e5efe3fa3829cdcb88f
swang99 <35492791+swang99@users.noreply.github.com>
2018-09-07 10:21:25 -0400

    Updated from Brython Server: 9/7/2018 10:21:22 AM

--- mmproj1.py
+++ mmproj1.py
@@ -1,5 +1,7 @@
 from random import randint
 
+notlookedat = 0
+
 L = [1]
 n = int(input('Enter a number of birds: '))
 
@@ -9,5 +11,10 @@ for i in range(1, n+1):
 L.append(2)
 print(L)
 
+for i in range(1, n+1):
+    if L[i-1] == 2 and L[i+1] == 1:
+        notlookedat = notlookedat + 1
+
+print(notlookedat)
 
 
