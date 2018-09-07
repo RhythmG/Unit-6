d6e11080ed943c61084a02caf888b59b18d80dd9
swang99 <35492791+swang99@users.noreply.github.com>
2018-09-07 17:25:20 -0400

    Updated from Brython Server: 9/7/2018 5:25:20 PM

--- mmproj1.py
+++ mmproj1.py
@@ -1,34 +1,37 @@
+#Math Modeling Proj #1
+#Problem 5: Birds on a Wire
+
 from random import randint
 
-notlookedat = 0
-j= 0
-k= 0
-l= 0
-
-L = [1]
-n = int(input('Enter a number of birds: '))
-
-for i in range(1, n+1):
-    L.append(randint(1,2))
-
-L.append(2)
-print(L)
-
-for i in range(1, n):
-    if L[i-1] == 2 and L[i+1] == 1:
+def birds():
+    notlookedat = 0
+    j= 0
+    k= 0
+    l= 0
+    
+    L = [1]
+    n = int(input('Enter a number of birds: '))
+    
+    for i in range(1, n+1):
+        L.append(randint(1,2))
+    
+    L.append(2)
+    print(L)
+    
+    for i in range(1, n):
+        if L[i-1] == 2 and L[i+1] == 1:
+            notlookedat = notlookedat + 1
+            j = j+1
+    if L[0] == 1 and L[1] == 1:
         notlookedat = notlookedat + 1
-        j = j+1
+        k = k+1
+    if L[n] == 2 and L[n+1] == 2:
+        notlookedat = notlookedat + 1
+        l = l+1
+    
+    print("Not looked at:", notlookedat)
+    print(j,k,l)
 
-if L[0] == 1 and L[1] == 1:
-    notlookedat = notlookedat + 1
-    k = k+1
-if L[n] == 2 and L[n+1] == 2:
-    notlookedat = notlookedat + 1
-    l = l+1
-
-print("Not looked at:", notlookedat)
-print(j)
-print(k)
-print(l)
-
+for i in range(1, 10):
+    birds()
 
