48e48c3eba0549b8513e7cba5588f7794d1d863a
swang99 <35492791+swang99@users.noreply.github.com>
2018-10-17 19:48:05 -0400

    Update Repeatingdigits.py

--- Repeatingdigits.py
+++ Repeatingdigits.py
@@ -1,1 +1,16 @@
+#Stephen Wang
+#Math Modeling Project 3, Homework Question 1
+
+from math import gcd()
+
+print("Enter a fraction in the form of A/B")
+numerator = int(input("Enter your A value: "))
+denominator = int(input("Enter your B value: "))
+
+
+#Case 1: whole numbers
+if numerator%denominator == 0:
+    print("Your fraction in decimal form has no non-repeating or repeating digits.")
+
+math.gcd(numerator, denominator)
 
