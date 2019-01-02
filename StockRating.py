da2f8ec2322772bd52086bbe577080a0ae8f39a7
swang99 <35492791+swang99@users.noreply.github.com>
2019-01-02 09:58:44 -0500

    Updated from Brython Server: 1/2/2019 9:58:43 AM

--- StockRating.py
+++ StockRating.py
@@ -1,1 +1,14 @@
+#Stephen Wang
+#Math Modeling: Stock Rating System
+
+#marketcap = shares * price
+#totaldebt = stdebt + ltdebt
+#+ Minority Shares
+#+ Preferred Equity
+#- Cash & Short-Term Investments 
+#Enter EBITDA
+#Enter the ratio for 4 or 5 other competitors
+#% diff EV: EBITDA to competitor average
+
+
 
