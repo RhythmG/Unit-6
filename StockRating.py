953aa0ee9bd41564b6602830952adb4b69893788
swang99 <35492791+swang99@users.noreply.github.com>
2019-01-02 10:08:34 -0500

    Updated from Brython Server: 1/2/2019 10:08:33 AM

--- StockRating.py
+++ StockRating.py
@@ -10,15 +10,25 @@
 #Enter the ratio for 4 or 5 other competitors
 #% diff EV: EBITDA to competitor average
 
+from math import *
+
 def Step1(): #EV to EBITDA ratio rating
     print("Welcome to the Stock Rating System!")
-    shares = print(input("To start, please enter the company's current shares outstanding: "))
-    price = print(input("Next, input the stock's current price: "))
+    shares = input("To start, please enter the company's current shares outstanding: (in billions) ")
+    price = input("Next, input the stock's current price: ")
     marketcap = shares * price
     print("We are done with market capitalization. Now let's get information on the company's debt.")
     stdebt = print(input("Now, input the company's short term debt: "))
     ltdebt = print(input("Now, input the company's long term debt: "))
     totaldebt = stdebt + ltdebt
+    minorinter = print(input("Now, input the company's minority interest: "))
+    prfereq = print(input("Now, input the company's preferred equity: "))
+    cashoinv = print(input("Now, input the company's total cash and short-term investments: "))
+    EV = marketcap + totaldebt + minorinter + prfereq - cashoinv
+
+Step1()
+print(EV)
+    
 
 
 
