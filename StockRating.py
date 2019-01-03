31430087e39bb33f9f7c8603c8ea4725080ee4ec
swang99 <35492791+swang99@users.noreply.github.com>
2019-01-03 10:20:35 -0500

    Updated from Brython Server: 1/3/2019 10:20:34 AM

--- StockRating.py
+++ StockRating.py
@@ -18,13 +18,19 @@ def Step1(): #EV to EBITDA ratio rating
     price = float(input("Next, input the stock's current price: "))
     marketcap = shares * price
     print("We are done with market capitalization. Now let's get information on the company's debt.")
-    stdebt = print(input("Now, input the company's short term debt: "))
-    ltdebt = print(input("Now, input the company's long term debt: "))
+    stdebt = float(input("Now, input the company's short term debt: "))
+    ltdebt = float(input("Now, input the company's long term debt: "))
     totaldebt = stdebt + ltdebt
-    minorinter = print(input("Now, input the company's minority interest: "))
-    prfereq = print(input("Now, input the company's preferred equity: "))
-    cashoinv = print(input("Now, input the company's total cash and short-term investments: "))
+    minorinter = float(input("Now, input the company's minority interest: "))
+    prfereq = float(input("Now, input the company's preferred equity: "))
+    cashoinv = float(input("Now, input the company's total cash and short-term investments: "))
     EV = marketcap + totaldebt + minorinter + prfereq - cashoinv
+    competratio1 = float(input("Enter the EV:EBITDA ratio for the 1st competitor of the stock: "))
+    competratio2 = float(input("Enter the EV:EBITDA ratio for the 1st competitor of the stock: "))
+    competratio3 = float(input("Enter the EV:EBITDA ratio for the 1st competitor of the stock: "))
+    competratio4 = float(input("Enter the EV:EBITDA ratio for the 1st competitor of the stock: "))
+    competratio = (competratio1+competratio2+competratio3+competratio4)
+    
 
 Step1()
 print(EV)
