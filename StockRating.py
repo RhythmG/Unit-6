ab682cf2a8cd0967bc467c32ac2969e608351059
swang99 <35492791+swang99@users.noreply.github.com>
2019-07-10 23:55:19 -0400

    Updated from Brython Server: 7/10/2019 11:55:19 PM

--- StockRating.py
+++ StockRating.py
@@ -29,12 +29,12 @@ def Step1(): #EV:EBITDA Rating
     EV = marketcap + totdebt + minoi + prfeq - cashinv
     EBITDA = float(input("Enter the current EBITDA for this stock: "))
     step1ratio = EV/EBITDA
-    cptratio1 = float(input("Enter the EV:EBITDA ratio for the 1st competitor of the stock: "))
-    cptratio2 = float(input("Enter the EV:EBITDA ratio for the 2nd competitor of the stock: "))
-    cptratio3 = float(input("Enter the EV:EBITDA ratio for the 3rd competitor of the stock: "))
-    cptratio4 = float(input("Enter the EV:EBITDA ratio for the 4th competitor of the stock: "))
-    cptratio5 = float(input("Enter the EV:EBITDA ratio for the 5th competitor of the stock: "))
-    competoverall = (cptratio1 + cptratio2 + cptratio3 + cptratio4 + cptratio5)/5
+    cptr1 = float(input("Enter the EV:EBITDA ratio for the 1st competitor of the stock: "))
+    cptr2 = float(input("Enter the EV:EBITDA ratio for the 2nd competitor of the stock: "))
+    cptr3 = float(input("Enter the EV:EBITDA ratio for the 3rd competitor of the stock: "))
+    cptr4 = float(input("Enter the EV:EBITDA ratio for the 4th competitor of the stock: "))
+    cptr5 = float(input("Enter the EV:EBITDA ratio for the 5th competitor of the stock: "))
+    competoverall = (cptr1 + cptr2 + cptr3 + cptr4 + cptr5)/5
     step1pdiff = ((step1ratio - competoverall)/competoverall)*100
     rating1 = (-0.125*step1pdiff) + 5
     print("")
