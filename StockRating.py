bad80b0c94aaefcf4a7e1f292c7e09771132e407
swang99 <35492791+swang99@users.noreply.github.com>
2019-07-12 15:06:45 -0400

    Updated from Brython Server: 7/12/2019 3:06:45 PM

--- StockRating.py
+++ StockRating.py
@@ -65,7 +65,7 @@ def Step3(): #VaR Rating
     meanchange = float(input("Mean % change in stock price:"))
     stdchange = float(input("Standard deviation % change in stock price:"))
     time = float(input("Specify a time frame to forecast this stock (short-term recommended):"))
-    gbm = price * exp(stdchange*sqrt(t)*random.uniform(0,1) + meanchange*time #change to repeat 1000 times
+    gbm = price * exp(stdchange*sqrt(t)*random.uniform(0,1)) + meanchange*time #change to repeat 1000 times
     
 Step1()
 Step2()
