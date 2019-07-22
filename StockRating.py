c6e65d76fd405041d00d06c53ac67e3a524eb842
swang99 <35492791+swang99@users.noreply.github.com>
2019-07-22 17:17:59 -0400

    Update StockRating.py

--- StockRating.py
+++ StockRating.py
@@ -30,52 +30,50 @@ print("Step 1: Evaluates the stock in relation to its competitors.")
 print("Step 2: Performs DCF analysis on non-cyclical stocks.")
 print("Step 3: Looks at the future risk of the stock.")
   
-#EV:EBITDA Rating 
-    EV = marketcap + totdebt + minoi + prfeq - cashinv
-    EBITDA = 16.79 
-    s1ratio = EV/EBITDA
-    cptr1 = 7.68 #Competitor 1 EV:EBITDA
-    cptr2 = 10.80 #Competitor 2 EV:EBITDA
-    cptr3 = 15.22 #Competitor 3 EV:EBITDA
-    cptr4 = 11.29 #Competitor 4 EV:EBITDA
-    cptr5 = 12.37 #Competitor 5 EV:EBITDA
-    competoverall = (cptr1 + cptr2 + cptr3 + cptr4 + cptr5)/5 #Competitor Average
-    step1pdiff = ((s1ratio - competoverall)/competoverall)*100 # % diff w/ competitors
-    rating1 = (-0.125*step1pdiff) + 5 
-    print("")
-    """print(round(step1pdiff, 2), "%")"""
-    print('\033[1m' + "Step 1 Rating:", round(rating1, 2), "\n")
-    
+#Step 1: EV-to-EBITDA Rating 
+EV = marketcap + totdebt + minoi + prfeq - cashinv
+EBITDA = 16.79 
+s1ratio = EV/EBITDA
+cptr1 = 7.68 #Competitor 1 EV:EBITDA
+cptr2 = 10.80 #Competitor 2 EV:EBITDA
+cptr3 = 15.22 #Competitor 3 EV:EBITDA
+cptr4 = 11.29 #Competitor 4 EV:EBITDA
+cptr5 = 12.37 #Competitor 5 EV:EBITDA
+competoverall = (cptr1 + cptr2 + cptr3 + cptr4 + cptr5)/5 #Competitor Average
+step1pdiff = ((s1ratio - competoverall)/competoverall)*100 # % diff w/ competitors
+print("")
+rating1 = (-0.125*step1pdiff) + 5 
+
 #Have the following information at hand before running part 2:
 #Operating cash flow for the past 4 years
 
-#DCF Rating (Non-cyclical stocks only)
-    marketcap = 33.24972
-    avgoper = 3.77275 #average operating cash flow for past four years
-    print('\033[0m' "a) Consumer Cyclical, Tech, Basic Materials, Energy, Industrials", "\n", "b) Healthcare, Telecomm, Consumer Defense", "\n", "c) Finance")
-    sector = str(input("Please indicate the letter containing the sector in which your stock falls into."))
-    n = 20
-    if sector == "a":
-        print("Because your stock is cyclical, we will skip to the last step.")
-        Step3()
-    elif sector == "b":
-        dcf = sum([avgoper/(1+0.06)**(i) for i in range(1,n+1)])
-        print("DCF: ", dcf)
-    elif sector == "c":
-        dcf = sum([avgoper/(1+0.12)**(i) for i in range(1,n+1)])
-    print("DCF (in billions): ", dcf)
-    step2pdiff = round(((dcf-marketcap)/marketcap)*100,2)
-    print("% diff from market cap: ", step2pdiff)
-    rating2 = (0.133*step2pdiff) + 6.667
-    print('\033[1m' + "Step 2 Rating: ", round(rating2, 2))
+#Step 2: DCF Rating (Non-cyclical stocks only)
+marketcap = 33.24972
+avgoper = 3.77275 #average operating cash flow for past four years
+print('\033[0m' "a) Consumer Cyclical, Tech, Basic Materials, Energy, Industrials", "\n", "b) Healthcare, Telecomm, Consumer Defense", "\n", "c) Finance")
+sector = str(input("Please indicate the letter containing the sector in which your stock falls into."))
+print('\n')
+n = 20
+if sector == "a":
+    print("Because your stock is cyclical, we will skip to the last step.")
+    Step3()
+elif sector == "b":
+    dcf = sum([avgoper/(1+0.06)**(i) for i in range(1,n+1)])
+    print("DCF: ", dcf)
+elif sector == "c":
+    dcf = sum([avgoper/(1+0.12)**(i) for i in range(1,n+1)])
+step2pdiff = round(((dcf-marketcap)/marketcap)*100,2)
+rating2 = (0.133*step2pdiff) + 6.667
+print('\033[1m' + "Step 1 Rating:", round(rating1, 2))
+print('\033[1m' + "Step 2 Rating: ", round(rating2, 2))
 
-#VaR Rating
-    time = 756 #number of days in historical data
-    fifth = ceil(time*0.05)
-    sum = -1.2705 #sum of daily % changes up to 5th percentile 
-    c_var = (1/fifth)*sum*100 #Expected shortfall, taken from the 5th %-tile of the historical % change 
-    rating3 = c_var + 10
-    print('\033[1m' + "Step 3 Rating: ", round(rating3, 2))
+#Step 3: Expected Shortfall Rating
+time = 756 #number of days in historical data
+fifth = ceil(time*0.05)
+sum = -1.2705 #sum of daily % changes up to 5th percentile 
+c_var = (1/fifth)*sum*100 #Expected shortfall, taken from the 5th %-tile of the historical % change 
+rating3 = c_var + 10
+print('\033[1m' + "Step 3 Rating: ", round(rating3, 2),'\n')
     
 final_rating = (rating1 + rating2 + rating3)/3
 print('\033[1m' + "Final Rating: ", round(final_rating, 2))
